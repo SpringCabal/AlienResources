224e7695bf107e0cc6448747cf7ea6717ace859c
Anarchid <anarchid@mail.ru>
2015-08-23 15:38:13 +0300

    add missing building1 textures and building2 final model

---
+++ Objects3d/building1.dae.lua
@@ -0,0 +1,6 @@
+model = {
+      tex1 = "building1.png",
+      tex2 = "building1_s.png",
+      --invertteamcolor = false
+}
+return model
---
+++ Objects3d/building2.dae.lua
@@ -0,0 +1,6 @@
+model = {
+      tex1 = "building2.png",
+      tex2 = "building2_s.png",
+      --invertteamcolor = false
+}
+return model
