860d622fa3b4ff8e44267206d680c93735714abf
Anarchid <anarchid@mail.ru>
2015-08-25 01:53:58 +0300

    sickest fires

--- effects/sickfires.lua
+++ effects/sickfires.lua
@@ -68,7 +68,7 @@ return {
       water              = true,
 	  air                = true,
       properties = {
-        delay              = [[d0.05y1 i1x1 r10 -5]],
+        delay              = [[d0.05y1 i1x1 r20 -10]],
         dir                = [[dir]],
         explosiongenerator = [[custom:fire]],
         pos                = [[0, 0, 0]],
@@ -92,7 +92,7 @@ return {
         pos                = [[r20 -10,r20 -10,r20 -10]],
         size               = [[30]],
         sizegrowth         = [[0.4]],
-        speed              = [[0, 0.2, 0]],
+        speed              = [[0, 4r4, 0]],
         texture            = [[flame]],
       },
     },
